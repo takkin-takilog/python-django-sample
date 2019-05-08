fce87b469dc8d1005a3f9a4b92b3d38a9022cc20
takkin <42467940+takkin-takilog@users.noreply.github.com>
2019-05-08 20:54:56 +0900

    [add] templates

--- app_samp01/views.py
+++ app_samp01/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.generic.base import View
 
 # Create your views here.
+
+
+class AppView(View):
+
+    template_name = 'app_samp01/index.html'
+
+    myapp_data = {
+        'app': 'Django'
+    }
+
+    def get(self, request):
+        return render(request, self.template_name, self.myapp_data)
