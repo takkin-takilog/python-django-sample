4de0f858d6e1a1b28325647c615bb71e9648bbf2
takkin <42467940+takkin-takilog@users.noreply.github.com>
2019-05-11 22:10:14 +0900

    [update]

--- app_samp01/views.py
+++ app_samp01/views.py
@@ -5,8 +5,6 @@ from django.views.generic.base import TemplateView
 
 class AppViewIdx(TemplateView):
 
-    #template_name = 'app_samp01/index.html'
-
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["app"] = "Index"
@@ -14,8 +12,6 @@ class AppViewIdx(TemplateView):
 
 
 class AppViewPage01(TemplateView):
-
-    #template_name = 'app_samp01/page01.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
